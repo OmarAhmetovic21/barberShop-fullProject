2e1522aa809d0bd1a4dce09f4df81b715c943d85
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2022-05-15 20:32:14 +0200

    Finished Services page

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -3,6 +3,10 @@ import Sidebar from '../Sidebar';
 import React, { useState } from "react";
 import Header from '../common/Header';
 import haircutImg from '../../images/Haircut.jpg';
+import facewashImg from '../../images/Facewash.jpg';
+import beardImg from '../../images/Beard.jpg';
+import hairdyeImg from '../../images/Hair dye.jpg'
+
 
 export default function Services(){
 
@@ -22,9 +26,23 @@ return (
       <img src={haircutImg} style={{height: '50vh', width: '100vh', marginTop: '50px'}}></img>
      </div>
      <h3>Haircut: 10KM</h3>
+
+     <div>
+      <img src={facewashImg} style={{height: '50vh', width: '100vh', marginTop: '50px'}}></img>
+     </div>
+     <h3>Face wash: 5KM</h3>
+
+     <div>
+      <img src={beardImg} style={{height: '50vh', width: '100vh', marginTop: '50px'}}></img>
+     </div>
+     <h3>Beard: 7KM</h3>
+
+     <div>
+      <img src={hairdyeImg} style={{height: '50vh', width: '100vh', marginTop: '100px', }}></img>
+     </div>
+     <h3 style={{marginBottom: '50px'}}>Hair dye: 15KM</h3>
      <div>
       
-     
     </div>
     </div>
     
